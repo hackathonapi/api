1ca202a19c5ae2beecc93f440fda713f1c14cb98
Dawid <dawidwas155@gmail.com>
2026-02-28 12:06:01 -0600

    Get Routes for Extractors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 from .extractor import extract, ExtractRequest, ExtractionResult
 
@@ -10,6 +10,11 @@ def root():
     return {"message": "API is running!"}
 
 
+@app.get("/extract", response_model=ExtractionResult)
+async def extract_get(url: str = Query(None), text: str = Query(None)) -> ExtractionResult:
+    return await extract(ExtractRequest(url=url, text=text))
+
+
 @app.post("/extract", response_model=ExtractionResult)
-async def extract_route(request: ExtractRequest) -> ExtractionResult:
+async def extract_post(request: ExtractRequest) -> ExtractionResult:
     return await extract(request)
